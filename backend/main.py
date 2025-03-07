3892df17755ac217539cd4b19a44222d11d89731
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-03-08 00:40:37 +0500

    Add initial Flask application setup with SQLAlchemy integration and Recipe model

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,49 @@
 from flask import Flask
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, fields
 from config import DevConfig
+from models import Recipe
+from exts import db
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+
+db.init_app(app)
+
 api = Api(app, doc='/docs')
+
+recipe_model=api.model(
+    "Recipe",
+    {
+        "id": fields.Integer,
+        "name": fields.String,
+        "description": fields.String,
+    }
+)
 
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
         return {'message': 'hello world'}
     
+@api.route('/recipes')
+class RecipesResource(Resource):
+    def get(self):
+        pass
+    def post(self):
+        pass
+
+@api.route('/recipe/<int:id>')
+class RecipeResource(Resource):
+    def get(self, id):
+        pass
+    def put(self, id):
+        pass
+    def delete(self, id):
+        pass
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Recipe': Recipe}
 
 if __name__ == '__main__':
-      app.run(debug=True)
+    app.run(debug=True)
---
+++ backend/models.py
@@ -0,0 +1,30 @@
+from exts import db
+
+"""
+class Recipe:
+   id: int primary key
+   title: str
+   description: str (text)
+   ingredients: str
+"""
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"<Recipe {self.title} >"  
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, title, description):
+         self.title = title
+         self.description = description
+         db.session.commit()
