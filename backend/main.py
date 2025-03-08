f77e8cebe07c9f2405ea9a94348433fdf905feb9
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-03-08 16:56:59 +0500

    Add initial migration setup for Flask application with Alembic

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restx import Api, Resource, fields
+from flask_migrate import Migrate
 from config import DevConfig
 from models import Recipe
 from exts import db
@@ -8,6 +9,7 @@ app = Flask(__name__)
 app.config.from_object(DevConfig)
 
 db.init_app(app)
+migrate = Migrate(app, db)
 
 api = Api(app, doc='/docs')
 
@@ -15,7 +17,7 @@ recipe_model=api.model(
     "Recipe",
     {
         "id": fields.Integer,
-        "name": fields.String,
+        "title": fields.String,
         "description": fields.String,
     }
 )
@@ -27,19 +29,39 @@ class HelloResource(Resource):
     
 @api.route('/recipes')
 class RecipesResource(Resource):
+    @api.marshal_list_with(recipe_model)
     def get(self):
-        pass
+        recipes = Recipe.query.all()
+        return recipes
+    
+    @api.marshal_with(recipe_model)
     def post(self):
-        pass
+        data = request.get_json()
+        new_recipe = Recipe(
+            title=data.get('title'),
+            description=data.get('description')
+        )
+        new_recipe.save()
+        return new_recipe, 201
 
 @api.route('/recipe/<int:id>')
 class RecipeResource(Resource):
+
+    @api.marshal_with(recipe_model)
     def get(self, id):
-        pass
+        recipe = Recipe.query.get_or_404(id)
+        return recipe
+    
+    @api.marshal_with(recipe_model)
     def put(self, id):
-        pass
+        recipe_to_Update = Recipe.query.get_or_404(id)
+        data = request.get_json()
+        recipe_to_Update.update(data.get('title'), data.get('description'))
+        return recipe_to_Update
     def delete(self, id):
-        pass
+        recipe_to_delete = Recipe.query.get_or_404(id)
+        recipe_to_delete.delete()
+        return None, 204
 
 @app.shell_context_processor
 def make_shell_context():
