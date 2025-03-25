7f86e0a8626d642cdc3ac606823e72406dc431b0
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-03-25 21:42:46 +0500

    Add authentication provider using react-token-auth for managing user sessions

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,41 @@
-import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../auth";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
 
-    const [formData, setFormData] = useState({
-      username: "",
-      password: "",
-    });
-    const { username, password} = formData;
-    const handleChange = (e) => {
-      setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log("form-submit");
-      formData.username = "";
-      formData.password = "";
-      setFormData({ ...formData });
+  const loginUser = (data) => {
+    console.log("Form submitted:", data);
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }
+
+    fetch('/auth/login',requestOptions)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data.access_token)
+        
+        if (data){
+         login(data.access_token)
+
+         navigate('/')
+        }
+        else{
+            alert('Invalid username or password')
+        }
+
+
+      })
     }
 
   return (
@@ -30,35 +48,37 @@ const Login = () => {
             <Form.Control
               type="text"
               placeholder="Enter username"
-              value={username}
-              name="username"
-              onChange={handleChange}
+              {...register("username", { required: true, maxLength: 25 })}
             />
           </Form.Group>
+          {errors.username && <p style={{color: "red"}}>Username is required</p>}
+          {errors.username?.type === "maxLength" && <p style={{color: "red"}}>Username is too long maxLength 25</p>}
           <br />
           <Form.Group>
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
               placeholder="Enter your password"
-              value={password}
-              name="password"
-              onChange={handleChange}
+              {...register("password", { required: true, maxLength: 20 })}
             />
           </Form.Group>
+          {errors.password && <p style={{color: "red"}}>Password is required</p>}
+          {errors.password?.type === "maxLength" && <p style={{color: "red"}}>Password is too long maxLength 20</p>}
           <br />
-            <Button
-              variant="primary"
-              className="mt-3"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              Login
-            </Button>
-            <br />
-            <Form.Group>
-              <small>Do not have an account ? <Link to={"/signup"}>Sign up</Link></small>
-            </Form.Group>
+          <Button
+            variant="primary"
+            className="mt-3"
+            type="submit"
+            onClick={handleSubmit(loginUser)}
+          >
+            Login
+          </Button>
+          <br />
+          <Form.Group>
+            <small>
+              Do not have an account ? <Link to={"/signup"}>Sign up</Link>
+            </small>
+          </Form.Group>
         </Form>
       </div>
     </div>
